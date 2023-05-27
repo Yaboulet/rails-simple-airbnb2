b1fdcdd3ae169d11ecf713c650a31e2867f91191
Youssef Aboulethar <garbarc@gmail.com>
2023-05-27 15:54:42 +0200

    xx

---
+++ config/initializers/unplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "-uPuv3USn_oyLMWoQfMqbAebYYW60Vu0EndlVGiW5mA"
+  config.application_secret = "noiVn-dOjnOeglkIGhNXUxg4gzCiyqwkaRinNJ1oM_s"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.utm_source = "Student application"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+12.times do
+  # Generate fake data for each attribute
+  name = Faker::Address.community
+  address = Faker::Address.full_address
+  description = Faker::Lorem.paragraph(sentence_count: 3)
+  price_per_night = Faker::Number.between(from: 50, to: 300)
+  number_of_guests = Faker::Number.between(from: 1, to: 6)
+
+  # Create a Flat instance using the generated data
+  Flat.create!(
+    name: name,
+    address: address,
+    description: description,
+    price_per_night: price_per_night,
+    number_of_guests: number_of_guests
+  )
+end
